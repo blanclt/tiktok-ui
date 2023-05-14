5dc0a4d671d5c84a112905c8f6dd08e9a58d7dd1
Your Name <you@example.com>
2023-05-14 12:29:01 +0700

    ok

---
+++ src/components/Layout/components/Header/index.js
@@ -0,0 +1,30 @@
+import images from '~/assets/images';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faSearch } from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
+function Header() {
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Tìm kiếm" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
